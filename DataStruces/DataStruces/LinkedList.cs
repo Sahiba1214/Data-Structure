1d54e8f6442b8d94f08b824d0eeacdbf0a70a3f7
sahiba <sahiba1214@gmail.com>
2021-11-22 17:41:38 +0530

    [sahiba1214] add : file added

--- DataStruces/DataStruces/LinkedList.cs
+++ DataStruces/DataStruces/LinkedList.cs
@@ -4,49 +4,20 @@ using System.Text;
 
 namespace DataStructures
 {
-    class LinkedList
-    { 
-        internal Node head;
-        ///<summary>
-        ///UC-1
-        /// </summary>
-        ///<param name="data"></param>
-        internal void Add(int data)
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
         {
-            Node node = new Node(data);
-            if (this.head == null)
-            {
-                this.head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine("{0} inserted into linked list", node.data);
+            this.data = data;
         }
-        internal void Display()
-        {
-            Node temp = this.head;
-            if (temp == null)
-            {
-                Console.WriteLine("Linked List is Empty");
-                return;
-            }
-            while (temp != null)
-            {
-                Console.WriteLine(temp.data + " ");
-                temp = temp.next;
-            }
-        }
+
     }
 }
 
 
 
-    
+
+
 
