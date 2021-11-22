539581a376026ce8509e3339cb795f2631221126
sahiba <sahiba1214@gmail.com>
2021-11-22 18:18:47 +0530

    [sahiba1214] add : file added

--- DataStruces/DataStruces/LinkedList.cs
+++ DataStruces/DataStruces/LinkedList.cs
@@ -1,18 +1,70 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataStructures
 {
-    public class Node
+    public class LinkedList
     {
-        public int data;
-        public Node next;
-        public Node(int data)
+        internal Node head;
+        internal void Add(int data)
         {
-            this.data = data;
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted into Linked List", node.data);
         }
-
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
     }
 }
 
